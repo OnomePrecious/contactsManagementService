cdd8587ead1a88ab50c17abaa61dbab86cec023c
Precious <preciousonome002@gmail.com>
2024-06-24 21:53:44 +0100

    contactsManagement

---
+++ src/main/java/org/example/dtos/response/ChangePasswordResponse.java
@@ -0,0 +1,12 @@
+package org.example.dtos.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ChangePasswordResponse {
+    private String message;
+    private String username;
+}
--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -1,7 +1,9 @@
 package org.example.services;
 
 import org.example.data.models.User;
+import org.example.dtos.request.ChangePasswordRequest;
 import org.example.dtos.request.RegisterRequest;
+import org.example.dtos.response.ChangePasswordResponse;
 import org.example.dtos.response.RegisterResponse;
 
 public interface UserService {
@@ -9,4 +11,8 @@ public interface UserService {
     RegisterResponse registerUser (RegisterRequest request);
 
     User findUserByUsername (String username);
+
+    ChangePasswordResponse changePassword (ChangePasswordRequest changePasswordRequest);
+
+
 }
