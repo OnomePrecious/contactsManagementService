682185958aa83f9fe99c036b33e2e2684eca7f8c
Precious <preciousonome002@gmail.com>
2024-06-24 22:22:55 +0100

    contactsManagement

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,7 +1,24 @@
 package org.example.controllers;
 
+import org.example.dtos.request.RegisterRequest;
+import org.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/contacts")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/Register")
+    public ResponseEntity<?> RegisterUser(@RequestBody RegisterRequest request){
+
+
+    }
+
 }
