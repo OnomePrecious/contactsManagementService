e32738745b01a511191e8d55082b838e41334896
Precious <preciousonome002@gmail.com>
2024-06-24 21:54:25 +0100

    contactsManagement

--- src/main/java/org/example/dtos/request/ChangePasswordRequest.java
+++ src/main/java/org/example/dtos/request/ChangePasswordRequest.java
@@ -1,4 +1,13 @@
 package org.example.dtos.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class ChangePasswordRequest {
+    private String username;
+    private String password;
+    private String newPassword;
 }
