5440d07727b90da1532c39a0f74c8998ceca7846
Precious <preciousonome002@gmail.com>
2024-06-26 10:05:32 +0100

    contactsManagement

--- src/test/java/org/example/services/ContactServiceImplTest.java
+++ src/test/java/org/example/services/ContactServiceImplTest.java
@@ -63,4 +63,34 @@ class ContactServiceImplTest {
             contactService.createNewContact(createNewContactRequest);
         });
     }
+
+    @Test
+    public void testThatRegisteredUserCanCreateTwoContact(){
+        RegisterRequest request = new RegisterRequest();
+        request.setFirstName("Precious");
+        request.setLastName("Onome");
+        request.setUsername("username");
+        request.setPassword("password");
+        request.setEmail("precious@gmail.com");
+        request.setPhoneNumber(123);
+        var user = userService.registerUser(request);
+
+
+        CreateNewContactRequest createNewContactRequest = new CreateNewContactRequest();
+        createNewContactRequest.setFirstName("Alicia");
+        createNewContactRequest.setLastName("Keys");
+        createNewContactRequest.setUsername(user.getUsername());
+        createNewContactRequest.setPhoneNumber(1243);
+        contactService.createNewContact(createNewContactRequest);
+
+        CreateNewContactRequest createNewContactRequest1 = new CreateNewContactRequest();
+        createNewContactRequest1.setFirstName("Bianca");
+        createNewContactRequest1.setLastName("Blanche");
+        createNewContactRequest1.setUsername(user.getUsername());
+        createNewContactRequest1.setPhoneNumber(33451);
+        contactService.createNewContact(createNewContactRequest1);
+
+        assertEquals(2, contactRepository.count());
+
+    }
 }
