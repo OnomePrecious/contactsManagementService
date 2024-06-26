1780e5c357454914667c7ec0f4d7094f02332e61
Precious <preciousonome002@gmail.com>
2024-06-26 09:38:12 +0100

    contactsManagement

--- src/test/java/org/example/services/ContactServiceImplTest.java
+++ src/test/java/org/example/services/ContactServiceImplTest.java
@@ -1,6 +1,9 @@
 package org.example.services;
 
 import org.example.data.repositories.ContactRepository;
+import org.example.dtos.request.CreateNewContactRequest;
+import org.example.dtos.request.RegisterRequest;
+import org.example.dtos.response.RegisterResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ContactServiceImplTest {
     @Autowired
     private ContactRepository contactRepository;
+    private UserService userService;
+    private ContactService contactService;
 
     @BeforeEach
     void setUp() {
@@ -20,5 +25,20 @@ class ContactServiceImplTest {
 
     @Test
     void createNewContact() {
+        RegisterRequest request = new RegisterRequest();
+        request.setFirstName("Precious");
+        request.setLastName("Onome");
+        request.setUsername("username");
+        request.setPassword("password");
+        request.setEmail("precious@gmail.com");
+        request.setPhoneNumber(123);
+        userService.registerUser(request);
+
+        CreateNewContactRequest createNewContactRequest = new CreateNewContactRequest();
+        createNewContactRequest.setFirstName("ALicia");
+        createNewContactRequest.setLastName("Keys");
+        createNewContactRequest.setUsername("Alike");
+        createNewContactRequest.setPhoneNumber(123);
+
     }
 }
